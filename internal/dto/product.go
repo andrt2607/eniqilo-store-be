d3983bf7e17f4e8bfe49eca08bb4ec45008b3333
frnhsn <frn1006@gmail.com>
2024-05-10 15:54:32 +0700

    ops: add docker compose to run postgresql

--- internal/dto/product.go
+++ internal/dto/product.go
@@ -17,4 +17,16 @@ type (
 		ID        string `json:"id" validate:"omitempty"`
 		CreatedAt string `json:"createdAt" validate:"omitempty,iso8601"`
 	}
+
+	ReqUpdateProduct struct {
+		Name        string `json:"name" validate:"min=1,max=30"`
+		SKU         string `json:"sku" validate:"min=1,max=30"`
+		Category    string `json:"category" validate:"oneof=Clothing Accessories Footwear Beverages"`
+		ImageURL    string `json:"imageUrl" validate:"required"`
+		Notes       string `json:"notes" validate:"required,min=1,max=200"`
+		Price       int    `json:"price" validate:"required,min=1"`
+		Stock       int    `json:"stock" validate:"required,min=0,max=100000"`
+		Location    string `json:"location" validate:"required,min=1,max=200"`
+		IsAvailable bool   `json:"isAvailable" validate:"required"`
+	}
 )
